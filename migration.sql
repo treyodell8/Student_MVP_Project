101645d7445a30ff31b746298dc36f7857f40116
WayneOdell <treyodell8@gmail.com>
2022-05-24 17:30:32 -0500

    please kevin

--- migration.sql
+++ migration.sql
@@ -2,17 +2,18 @@
 
 -- CREATE DATABASE blogdb;
 
--- DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users;
 
--- DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS posts;
 
-CREATE TABLE posts (
-    postid SERIAL PRIMARY KEY,
-    post TEXT
-);
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    name TEXT,
-    postid INT REFERENCES posts(postid)
+    name TEXT
+);
+
+CREATE TABLE posts (
+    postid SERIAL PRIMARY KEY,
+    post TEXT,
+    userid INT REFERENCES users(id)
 );
--- seed.sql
+++ seed.sql
@@ -1,3 +1,3 @@
-INSERT INTO posts (post) VALUES ('This is supposed to be a really cool blog post');
+INSERT INTO users (name) VALUES ('trey');
 
-INSERT INTO users (name, postid) VALUES ('trey');
+INSERT INTO posts (post, userid) VALUES ('This is supposed to be a really cool blog post', 1);
