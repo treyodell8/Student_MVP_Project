5dd68d629bfeaae2ef141af256913ccc1db0a72f
WayneOdell <treyodell8@gmail.com>
2022-05-24 16:39:57 -0500

    secondary commitment

--- seed.sql
+++ seed.sql
@@ -1,5 +1,3 @@
-INSERT INTO users (name) VALUES ('trey');
+INSERT INTO posts (post) VALUES ('This is supposed to be a really cool blog post');
 
-
-
-INSERT INTO posts (post) VALUES ('This is supposed to be a really cool blog post')
+INSERT INTO users (name, postid) VALUES ('trey');
