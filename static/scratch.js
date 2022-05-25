be2de7858248892b50c74fe123db0614a9eda4a5
WayneOdell <treyodell8@gmail.com>
2022-05-25 14:17:21 -0500

    first commit with changes to front end

--- static/scratch.js
+++ static/scratch.js
@@ -1,3 +1,22 @@
 console.log("hello")
+const postBody = document.querySelector(".body-posts");
+$(postBody).hide();
+//Buttons for the nav-bar
+const bodyDashboard = document.querySelector('.body-dashboard');
+const postsBtn = document.querySelector('#posts');
+$(postsBtn).click(function(){
+    $(bodyDashboard).hide();
+})
 
-const allPostsBtn = document.getElementById("posts")
+$(postsBtn).click(function(){
+    $(postBody).show();
+})
+
+
+dataReturn()
+
+function dataReturn() {
+$.get("http://localhost:3000/api/users", function(data) {
+    console.log(data[0].name);
+})
+}
