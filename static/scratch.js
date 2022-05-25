2f8969e7ffc9ecc6dd692dfa1c321622bb0d48cd
WayneOdell <treyodell8@gmail.com>
2022-05-25 10:40:41 -0500

    changed href to /api/posts

--- static/scratch.js
+++ static/scratch.js
@@ -1,1 +1,8 @@
 console.log("hello")
+
+const allPostsBtn = document.getElementById("posts");
+if(allPostsBtn) {
+allPostsBtn.addEventListener('click', function() {
+    console.log("Nice Job")
+    })
+}
